5d112fe9ed5da1e6e0e8b09672f9bd3743c9a52b
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2019-07-18 18:58:05 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+const int width = 20;
+const int height = 20;
+
+int x, y, fruitX, fruitY, score;
+bool gameOver;
+enum eDirection {STOP = 0, LEFT, RIGHT, UP, DOWN};
+eDirection dir;
+*/
+
+int main()
+{
+    /*
+    setup();
+    while(!gameOver)
+    {
+        draw();
+        input();
+        logic();
+    }
+    */
+
+    return 0;
+}
+
+/*
+void setup()
+{
+    gameOver = false;
+    dir = STOP;
+    x = width / 2;
+    y = height / 2;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void draw()
+{
+    system("cls");          // clear the screen
+
+    for(int i = 0; i < width; i++)
+        cout << '#';
+
+    cout << endl;
+
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            if(j == 0)
+                cout << '#';
+            else
+                cout << ' ';
+            
+            if(j == width - 1)
+                cout << '#';
+        }
+    }
+
+    for(int i = 0; i < width; i++)
+        cout << '#';
+    
+    cout << endl;
+}
+
+void input()
+{
+
+}
+
+void logic()
+{
+
+}
+*/
