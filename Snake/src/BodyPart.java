84c3b72e1b1088dc4ef094b59d27943e57e75d67
Matt Smith <43058533+m-smit474@users.noreply.github.com>
2019-07-19 17:09:30 -0600

    Added Snake
    There is now a "snake" in the screen that moves

---
+++ Snake/src/BodyPart.java
@@ -0,0 +1,25 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class BodyPart {
+	
+	private int xCoor, yCoor, width, height;
+
+	public BodyPart(int xCoor, int yCoor, int tileSize) {
+		this.xCoor = xCoor;
+		this.yCoor = yCoor;
+		width = tileSize;
+		height = tileSize;
+	}
+	
+	public void tick() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		g.setColor(Color.BLACK);											// Border
+		g.fillRect(xCoor * width, yCoor * height, width, height);			// for snake
+		g.setColor(Color.GREEN);
+		g.fillRect(xCoor * width + 2, yCoor * height + 2, width - 4, height - 4);
+	}
+}
--- Snake/src/Screen.java
+++ Snake/src/Screen.java
@@ -1,28 +1,81 @@
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
 public class Screen extends JPanel implements Runnable{
 
-	private static final long serialVersionUID = 1L;
-	
+	private static final long serialVersionUID = 1L;			// Not sure what this does
+																// was recommended
 	public static final int WIDTH = 800, HEIGHT = 800;
 	private Thread thread;
 	private boolean running = false;
 	
+	private BodyPart b;
+	private ArrayList<BodyPart> snake;							// A dynamic array
+	
+	private int xCoor = 10, yCoor = 10;
+	private int size = 5;
+	
+	private boolean right = true,
+					left = false,
+					up = false,
+					down = false;
+	
+	private int ticks = 0;
+	
 	public Screen() {
 		setPreferredSize(new Dimension(WIDTH, HEIGHT));
+		
+		snake = new ArrayList<BodyPart>();
 		
 		start();
 	}
 	
 	public void tick() {										// Updates stuff
 		
+		if(snake.size() == 0) {
+			b = new BodyPart(xCoor, yCoor, 10);
+			snake.add(b);
+		}
+		
+		ticks++;
+		
+		if(ticks > 250000) {
+			if(right) xCoor++;
+			if(left) xCoor--;
+			if(up) yCoor--;
+			if(down) yCoor++;
+			
+			ticks = 0;
+			
+			b = new BodyPart(xCoor, yCoor, 10);
+			snake.add(b);
+			
+			if(snake.size() > size) {
+				snake.remove(0);
+			}
+		}
 	}
 	
 	public void paint(Graphics g) {								// JPanel component
 		
+		g.clearRect(0, 0, WIDTH, HEIGHT);						// Clears screen
+		
+		g.setColor(Color.BLACK);
+		for(int i = 0; i < WIDTH / 10; i++) {					// Vertical grid lines
+			g.drawLine(i * 10, 0, i * 10, HEIGHT);
+		}
+		
+		for(int i = 0; i < HEIGHT / 10; i++) {					// Horizontal grid lines
+			g.drawLine(0, i * 10, WIDTH, i*10);
+		}
+		
+		for(int i = 0; i < snake.size(); i++) {
+			snake.get(i).draw(g);
+		}
 	}
 	
 	public void start() {
